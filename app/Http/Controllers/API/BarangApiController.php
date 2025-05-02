a84ad0c716ece9eecb7a9ab0c54d69bf9652d6c9
Indokq <arsylfauzan19@gmail.com>
2025-05-02 23:13:07 +0700

    api update

--- app/Http/Controllers/API/BarangApiController.php
+++ app/Http/Controllers/API/BarangApiController.php
@@ -9,12 +9,27 @@ class BarangApiController extends Controller
 {
     public function index()
     {
-        $barangs = Barang::with('kategori')->get();
-
+        $barangs = Barang::with(['kategori', 'stok'])->get();
+    
+        $formatted = $barangs->map(function ($barang) {
+            return [
+                'id' => $barang->id,
+                'nama_barang' => $barang->nama_barang,
+                'deskripsi' => $barang->deskripsi,
+                'id_kategori' => $barang->id_kategori,
+                'kategori' => [
+                    'id' => $barang->kategori->id,
+                    'nama_kategori' => $barang->kategori->nama_kategori,
+                ],
+                'jumlah_tersedia' => $barang->stok ? $barang->stok->jumlah : 0,
+            ];
+        });
+    
         return response()->json([
             'success' => true,
-            'data' => $barangs
+            'data' => $formatted
         ]);
     }
+    
 }
 
--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\User;        
 use Illuminate\Support\Facades\Auth;
 
 
